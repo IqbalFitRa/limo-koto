845d188a39a2a466a2e545dcc3e879c6cb8c6696
IqbalFitRa <89772053+IqbalFitRa@users.noreply.github.com>
2023-07-10 22:05:58 +0700

    masuk

---
+++ User.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Lima Koto</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  </head>
+  <body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+        <div class="container">
+            <div class="mb-3">
+                <label for="namaUser" class="form-label">Nama :</label>
+                <input type="text" 
+                class="form-control" 
+                id="namaUser" 
+                placeholder="Input Nama">
+            </div>
+            <div class="mb-3">
+                <label for="nipUser" class="form-label">NIP :</label>
+                <input type="number" 
+                class="form-control" 
+                id="nipUser" 
+                placeholder="Input NIP">
+            </div>
+            <div class="mb-3">
+                <label for="alasanUser" class="form-label">Alasan :</label>
+                <input type="text" 
+                class="form-control" 
+                id="alasanUser" 
+                placeholder="Input NIP">
+            </div>
+        </div>
+  </body>
+</html>
