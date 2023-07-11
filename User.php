2d4ff315e9d8f88abb37c488de53d69475e43bda
Delia <deliajulianti07@gmail.com>
2023-07-11 13:05:38 +0700

    cetak surat

--- User.php
+++ User.php
@@ -5,48 +5,81 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Lima Koto</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-
     <style>
+        .print {
+            display: none;
+        }
         @media print {
-            .print , .button {
+            .print {
+                display: inline;
+            }
+            .button {
                 display: none;
             }
         }
     </style>
   </head>
   <body>
-    <a href="Print.php" class = "print">Print</a>
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Ambil data yang dikirim melalui formulir
+        $nama = $_POST['namaUser'];
+        $nip = $_POST['nipUser'];
+        $alasan = $_POST['alasanUser'];
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
-        <div class="container">
-            <div class="mb-3">
-                <label for="namaUser" class="form-label">Nama :</label>
-                <input type="text" 
-                class="form-control" 
-                id="namaUser" 
-                placeholder="Input Nama"
-                autocomplete="off">
-            </div>
-            <div class="mb-3">
-                <label for="nipUser" class="form-label">NIP :</label>
-                <input type="number" 
-                class="form-control" 
-                id="nipUser" 
-                placeholder="Input NIP"
-                autocomplete="off">
-            </div>
-            <div class="mb-3">
-                <label for="alasanUser" class="form-label">Alasan :</label>
-                <input type="text" 
-                class="form-control" 
-                id="alasanUser" 
-                placeholder="Input Alasan"
-                autocomplete="off">
-            </div>
-            <div>
-                <button type="submit"
-                class ="button">Submit</button>
-            </div>
+        // Lakukan validasi data jika diperlukan
+
+        // Simpan data ke dalam database atau lakukan operasi lain yang diinginkan
+        // ...
+
+        // Buat konten surat dalam variabel $surat
+        $surat = "
+        <h2>Data berhasil disimpan:</h2>
+        <p>Nama: $nama</p>
+        <p>NIP: $nip</p>
+        <p>Alasan: $alasan</p>";
+
+        // Simpan konten surat dalam file
+        $file = fopen('surat.html', 'w');
+        fwrite($file, $surat);
+        fclose($file);
+    }
+    ?>
+
+<div class="container">
+    <form action="surat.php" method="post">
+        <div class="mb-3">
+            <label for="namaUser" 
+            class="form-label">Nama :</label>
+            <input type="text" 
+            class="form-control" 
+            id="namaUser" 
+            name="namaUser" 
+            placeholder="Input Nama" 
+            autocomplete="off">
         </div>
-  </body>
-</html>
+        <div class="mb-3">
+            <label for="nipUser" 
+            class="form-label">NIP :</label>
+            <input type="number" 
+            class="form-control" 
+            id="nipUser" 
+            name="nipUser" 
+            placeholder="Input NIP" 
+            autocomplete="off">
+        </div>
+        <div class="mb-3">
+            <label for="alasanUser" 
+            class="form-label">Alasan :</label>
+            <input type="text" 
+            class="form-control" 
+            id="alasanUser" 
+            
+            name="alasanUser" 
+            placeholder="Input Alasan" autocomplete="off">
+        </div>
+        <div>
+            <button type="submit" class="button">Submit</button>
+        </div>
+    </form>
+</div>
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,25 +33,18 @@ class ComposerAutoloaderInit15a4ebf944ef288f875313fb4db19a90
 
         $loader->register(true);
 
-        $includeFiles = \Composer\Autoload\ComposerStaticInit15a4ebf944ef288f875313fb4db19a90::$files;
-        foreach ($includeFiles as $fileIdentifier => $file) {
-            composerRequire15a4ebf944ef288f875313fb4db19a90($fileIdentifier, $file);
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit15a4ebf944ef288f875313fb4db19a90::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
         }
 
         return $loader;
-    }
-}
-
-/**
- * @param string $fileIdentifier
- * @param string $file
- * @return void
- */
-function composerRequire15a4ebf944ef288f875313fb4db19a90($fileIdentifier, $file)
-{
-    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
-        require $file;
     }
 }
