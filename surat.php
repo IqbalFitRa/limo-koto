fcabf3f4307f0358cf6a625a8f6e9ea7b90b0b6f
Delia <deliajulianti07@gmail.com>
2023-07-11 21:47:23 +0700

    isi surat

--- surat.php
+++ surat.php
@@ -10,14 +10,44 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Buat konten surat dalam variabel $surat
     $surat = "
-    <h2>Surat Permohonan</h2>
+    <h2>PEMERINTAHAN NAGARI LIMO KOTO
+    KECAMATAN KOTO VII
+    JORONG KOTO PANJANG
+    </h2>
+    <h3>Alamat: Jorong Koto Panjang
+    ______________________________________________________________</h3>
+    <h4>=REKOMENDASI=
+    Nomor: 140/	/JR.KP-20…</h4>
+    
 
-    <p>Yang bertanda tangan di bawah ini:</p>
-    <p>Nama: $nama</p>
-    <p>NIP: $nip</p>
+    <p>Yang bertanda tangan dibawah ini :</p>
+    <p>Nama			: ANGGI KURNIAWAN</p>
+    <p>Jabatan		: KEPALA JORONG KOTO PANJANG</p>
+    <p>Dengan ini menerangkan bahwa	 :</p>
+    <p>Nama                 : $nama</p>
+    <p>Tempat / Tgl.Lahir   :</p>
+    <p>Jenis Kelamin        :Jenis Kelamin</p>
+    <p>Agama	            :Agama	</p>
+    <p>Pekerjaan            :Pekerjaan</p>
+    <p>Nip                  :$nip</p>
+    <p>PBB                  :alasan</p>
+    
 
-    <p>Dengan ini mengajukan permohonan dengan alasan sebagai berikut:</p>
-    <p>$alasan</p>";
+    <p>Nama tersebut diatas adalah benar-benar penduduk Jorong KOTO PANJANG Nagari Limo Koto, Kecamatan Koto VII, 
+    Kabupaten Sijunjung, selanjutnya yang bersangkutan akan mengurus Surat-surat Ke Kantor Wali Nagari Limo Koto antara lain:</p>
+    <p>Untuk Keperluan	
+    Demikianlah Rekomendasi ini kepada yang bersangkutan  untuk dipergunakan menurut semestinya, sesuai dengan peraturan yang berlaku</p>
+    
+    <p>1.$alasan</p>		
+    <p>2.</p>		
+    <p>3.</p>	
+    <p>4.</p>	
+    
+<p>Diberikan di Koto Panjang </p>          
+<p>Pada Tanggal:………………..20… </p>
+<p>KEPALA JORONG
+KOTO PANJANG</p>"
+;
 
     // Buat objek Dompdf
     $dompdf = new Dompdf();
